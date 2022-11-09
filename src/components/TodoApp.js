243d7f6551bb1403a73a9d44420ad46bca90723f
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-09 18:05:41 -0300

    Defined hook & components

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Typography } from '@mui/material';
-import Paper from '@mui/material';
-import AppBar from '@mui/material';
-import Toolbar from '@mui/material';
-import Grid from '@mui/material';
+import Paper from '@mui/material/Paper';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 
 export default function TodoApp() {
+  
+  const initialTodos = [
+    {id: 1, task: "Clean Fishtank", completed: false},
+    {id: 2, task: "Walk the dog", completed: true},
+    {id: 3, task: "Draw some art", completed: false},
+  ]
+  const [todos, setTodos] = useState(initialTodos)
+  const addTodo = newTodoText => {
+    setTodos([...todos, {id:4, task: newTodoText, completed: false}])
+  }
+
   return (
-    <div>TodoApp</div>
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa"
+      }}
+      elevation={0}    
+    >
+      <AppBar color="primary" position="static" style={{height: "64px"}}>
+        <Toolbar>
+          <Typography color="inherit">Todo with Hooks</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList todos={todos}/>
+    </Paper>
   )
 }
