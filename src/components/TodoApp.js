636e50482eb4be7217e08991b7d000981f3f03de
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-09 14:52:48 -0300

    Starter code

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+import Paper from '@mui/material';
+import AppBar from '@mui/material';
+import Toolbar from '@mui/material';
+import Grid from '@mui/material';
+
+
+export default function TodoApp() {
+  return (
+    <div>TodoApp</div>
+  )
+}
